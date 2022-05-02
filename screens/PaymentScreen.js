9a48a0b7062863e195c19870e9c5ca54180df3a9
Dhruv Ghadiali <86670107+21DG@users.noreply.github.com>
2022-05-03 02:17:12 +0530

    payment screen ui changes

--- screens/PaymentScreen.js
+++ screens/PaymentScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, Image, StyleSheet, TextInput, TouchableOpacity, Alert, Text } from "react-native";
+import { View, Image, StyleSheet, TextInput, TouchableOpacity, Alert, Text, ScrollView } from "react-native";
 import { CardField, useConfirmPayment } from "@stripe/stripe-react-native";
 
 //ADD localhost address of your server
@@ -68,69 +68,79 @@ const PaymentScreen = (props) => {
     };
 
     return (
-        <View style={styles.container}>
-            <View style={{ height: 50 }}>
-                <Text style={{ fontSize: 20, color: '#000000' }}>   {`Total Amount: \u00A3 ${totalPrice}`} </Text>
-            </View>
-            {showForm ?
-                <View>
-                    <TextInput
-                        autoCapitalize="none"
-                        placeholder="firstName"
-                        keyboardType="default"
-                        placeholderTextColor={'#1C2833'}
-                        value={firstName}
-                        onChange={value => setFirstName(value.nativeEvent.text)}
-                        style={styles.input} />
-                    <TextInput
-                        autoCapitalize="none"
-                        placeholder="lastName"
-                        keyboardType="default"
-                        placeholderTextColor={'#1C2833'}
-                        value={lastName}
-                        onChange={value => setLastName(value.nativeEvent.text)}
-                        style={styles.input} />
-                    <TextInput
-                        autoCapitalize="none"
-                        placeholder="address"
-                        keyboardType="default"
-                        placeholderTextColor={'#1C2833'}
-                        value={address}
-                        onChange={value => setAddress(value.nativeEvent.text)}
-                        style={styles.input} />
-                    <TextInput
-                        autoCapitalize="none"
-                        placeholder="postalCode"
-                        keyboardType="default"
-                        placeholderTextColor={'#1C2833'}
-                        value={postalCode}
-                        onChange={value => setPostalCode(value.nativeEvent.text)}
-                        style={styles.input} />
-                </View> : <View />}
-            <CardField
-                postalCodeEnabled={true}
-                placeholder={{
-                    number: "4242 4242 4242 4242",
-                }}
-                cardStyle={styles.card}
-                style={styles.cardContainer}
-                onCardChange={cardDetails => {
-                    setCardDetails(cardDetails);
-                }} />
-            <TouchableOpacity onPress={handlePayPress}>
-                <View style={{ marginRight: 5, alignItems: 'center', padding: 10 }}>
-                    <Image
-                        source={require('../assets/credit-card.png')}
-                        style={{
-                            flexDirection: 'row',
-                            flexWrap: 'wrap',
-                            resizeMode: 'contain',
-                            width: 40,
-                            height: 40,
-                        }} />
+        <ScrollView>
+            <View style={styles.container}>
+                <View style={{ height: 50 }}>
+                    <Text style={{ fontSize: 20, color: '#000000' }}>   {`Total Amount: \u00A3 ${totalPrice}`} </Text>
                 </View>
-            </TouchableOpacity>
-        </View>
+                {showForm ?
+                    <View style={{
+                        flex: 1,
+                        justifyContent: "center",
+                    }}>
+                        <TextInput
+                            autoCapitalize="none"
+                            placeholder="firstName"
+                            keyboardType="default"
+                            placeholderTextColor={'#1C2833'}
+                            value={firstName}
+                            onChange={value => setFirstName(value.nativeEvent.text)}
+                            style={styles.input} />
+                        <TextInput
+                            autoCapitalize="none"
+                            placeholder="lastName"
+                            keyboardType="default"
+                            placeholderTextColor={'#1C2833'}
+                            value={lastName}
+                            onChange={value => setLastName(value.nativeEvent.text)}
+                            style={styles.input} />
+                        <TextInput
+                            autoCapitalize="none"
+                            placeholder="address"
+                            keyboardType="default"
+                            placeholderTextColor={'#1C2833'}
+                            value={address}
+                            onChange={value => setAddress(value.nativeEvent.text)}
+                            style={styles.input} />
+                        <TextInput
+                            autoCapitalize="none"
+                            placeholder="postalCode"
+                            keyboardType="default"
+                            placeholderTextColor={'#1C2833'}
+                            value={postalCode}
+                            onChange={value => setPostalCode(value.nativeEvent.text)}
+                            style={styles.input} />
+                    </View> : <View />}
+                <View style={{
+                        flex: 1,
+                        justifyContent: "center",
+                    }}>
+                    <CardField
+                        postalCodeEnabled={true}
+                        placeholder={{
+                            number: "4242 4242 4242 4242",
+                        }}
+                        cardStyle={styles.card}
+                        style={styles.cardContainer}
+                        onCardChange={cardDetails => {
+                            setCardDetails(cardDetails);
+                        }} />
+                    <TouchableOpacity onPress={handlePayPress}>
+                        <View style={{ marginRight: 5, alignItems: 'center', padding: 10 }}>
+                            <Image
+                                source={require('../assets/credit-card.png')}
+                                style={{
+                                    flexDirection: 'row',
+                                    flexWrap: 'wrap',
+                                    resizeMode: 'contain',
+                                    width: 40,
+                                    height: 40,
+                                }} />
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </ScrollView>
     );
 };
 export default PaymentScreen;
@@ -139,9 +149,6 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: "center",
-        margin: 20,
-        marginTop: 200,
-        height: 500
     },
     input: {
         backgroundColor: "#efefefef",
